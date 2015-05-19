2ec1821eadbc3cd66c7b75224f840f3cd2b15cf7
Carlos <carlosrmd94@gmail.com>
2015-05-19 12:56:20 -0430

    Avance en lectura de teclas.

--- Main.hs
+++ Main.hs
@@ -9,9 +9,33 @@ import System.Environment
 
 ciclo :: Ventana -> Diagrama -> [Paso] -> IO ()
 ciclo v d pasos = do
-					putStrLn "Culo";
-					mostrar v [] d;
-					ciclo v d pasos
+					teclaleida <- leerTecla v;
+					case teclaleida of
+						Just tecla ->
+							case tecla of
+								"Up" 		-> do
+									putStrLn "Up!";
+									ciclo v d pasos
+								"Down" 		-> do
+									putStrLn "Down!";
+									ciclo v d pasos
+								"Right" 	-> do
+									putStrLn "Right!";
+									ciclo v d pasos
+								"Left" 		-> do
+									putStrLn "Left!";
+									ciclo v d pasos
+								"BackSpace" -> do
+									putStrLn "BackSpace!";
+									ciclo v d pasos
+								"q"			-> do
+									putStrLn "Bye";
+								_ 			-> do
+									putStrLn "Alguna otra.";
+									ciclo v d pasos
+									
+						Nothing ->
+							putStrLn "Culo"
 
 
 main :: IO ()
@@ -24,6 +48,7 @@ main =
 				case foo of
 					Right imagen -> do
 						v <- crearVentana (anchura imagen) (altura imagen);
+						mostrar v [] (Hoja(rectánguloImagen imagen)) 
 						ciclo v (Hoja(rectánguloImagen imagen)) []
 					Left razon -> putStrLn razon
 			_ -> putStrLn "Error en la entrada: Número de argumentos inválido."
